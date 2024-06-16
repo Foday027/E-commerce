538f79ff8ea02c0355eb811e8f84241fc411d8c2
Foday027 <kallon027@outlook.com>
2024-06-16 17:15:48 +0200

    e-commerce

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  useEffect(() => {
+    async function fetchData() {
+      let resp = await fetch('https://raw.githubusercontent.com/lukaszwos/4geeks-ecommerce/master/data.json');
+      let respReady = await resp.json();
+      setProducts(respReady);
+    }
+    fetchData();
+  }, [])
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState('');
+
+
+  const filteredProducts = search.length === 0 ? products :
+    products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
+
+
+
+  return (
+    <>
+
+      <form>
+        <label >
+          Find:
+        </label>
+        <input className='label mb-3' onChange={(e) => setSearch(e.target.value)
+        }
+          type='text'
+          placeholder='Search here'
+          value={search} />
+      </form>
+
+
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Category</th>
+            <th>Price</th>
+            <th>InStock</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {filteredProducts.map(product => (
+            <tr key={product.id} >
+              <td>{product.category}</td>
+              <td>{product.price}</td>
+              <td>{product.inStock ? 'yes' : 'no'} </td>
+              <td>{product.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App;
